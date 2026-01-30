ef39578a3e368f198b863caf6d4b82f20ab67216
Lucas Bottino <lucasgabottino@gmail.com>
2026-01-30 11:50:18 -0300

    feat: adiciona criacao de schemas no create.sql

--- scripts/create.sql
+++ scripts/create.sql
@@ -1,4 +1,8 @@
-CREATE TABLE stock_quotes (
+create schema if not exists bronze;
+create schema if not exists silver;
+create schema if not exists gold;
+
+CREATE TABLE public.stock_quotes (
   id SERIAL PRIMARY KEY,
   symbol TEXT,
   fetched_at BIGINT,
